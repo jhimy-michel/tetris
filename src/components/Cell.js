95b617200f9874ac30da779bfbc3af8d43ecc743
jhimy-michel <jhimy0riv@gmail.com>
2019-10-27 10:41:01 -0400

    tetris stage 1

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,10 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TRETOMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={'L'} color={TRETOMINOS['L'].color}>
+    Cell
+  </StyledCell>
+);
 export default Cell;
