0ffacbae02d7acaf5ccf7821a43406cadbfd4fea
jhimy-michel <jhimy0riv@gmail.com>
2022-09-24 21:06:36 +0200

    clear rows

--- src/hooks/useStage.ts
+++ src/hooks/useStage.ts
@@ -6,12 +6,26 @@ import { PlayerType } from "./usePlayer";
 export const useStage = (
   player: PlayerType,
   resetPlayer: any
-): [any[][], React.Dispatch<React.SetStateAction<any[][]>>] => {
+): [any[][], React.Dispatch<React.SetStateAction<any[][]>>, number] => {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    const sweepRows = (newStage: any) =>
+      newStage.reduce((acc: any, row: any) => {
+        if (row.findIndex((cell: any) => cell[0] === 0) === -1) {
+          setRowsCleared((prev) => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, "clear"]));
+          return acc;
+        }
+        acc.push(row);
+        return acc;
+      }, []);
+
     const updateStage = (prevStage: any[]) => {
-      // first flush the stage
+      // first flush the stages
       const newStage = prevStage.map((row) => row.map((cell: string[]) => (cell[1] === "clear" ? [0, "clear"] : cell)));
       // draw the tetromino
       player.tetramino.forEach((row, y) => {
@@ -25,11 +39,12 @@ export const useStage = (
       // then check if we collided
       if (player.collided) {
         resetPlayer();
+        return sweepRows(newStage);
       }
       return newStage;
     };
     setStage((prev) => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 };
