6000edd114c17ffb5f575ff122f234d11c2213ab
jhimy-michel <jhimy0riv@gmail.com>
2022-09-25 10:55:00 +0200

    adding react memo

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import { StyledCell } from './styles/StyledCell';
-import { TRETOMINOS } from '../tetrominos';
+import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TRETOMINOS } from "../tetrominos";
 
 const Cell = ({ type }: { type: number | string }) => <StyledCell type={type} color={TRETOMINOS[type].color} />;
 
-export default Cell;
+export default React.memo(Cell);
