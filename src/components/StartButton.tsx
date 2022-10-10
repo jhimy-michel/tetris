784b1c77d0f217c5561251286604a4ea4dd1e98a
jhimy-michel <jhimy0riv@gmail.com>
2022-10-10 19:51:54 +0200

    adding comments

--- src/components/StartButton.tsx
+++ src/components/StartButton.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { StyledStartButton } from './styles/StyledStartButton';
 
+/**
+ * Button that start the game
+ * @param param0 
+ * @returns 
+ */
 const StartButton = ({ callBack }: { callBack: () => void }) => (
   <StyledStartButton onClick={callBack}>Start Game</StyledStartButton>
 );
