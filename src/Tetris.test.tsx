8e76bc4084186111e27115d690cde05f781452d7
kaleidosim <info@kaleidosim.com>
2022-10-10 19:31:04 +0200

    updating snapshot test

--- src/Tetris.test.tsx
+++ src/Tetris.test.tsx
@@ -1,11 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Tetris from "./components/Tetris";
 
 describe("Testing Tetrix main component", () => {
-  it("it should render the game", () => {
+  it("should match snapshot", () => {
     const view = render(<Tetris />);
-    console.log(view.container)
-
     expect(view.container).toMatchSnapshot();
   });
 });
